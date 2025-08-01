4cee9346c93c1ca9e237698880a06cd9cfaf4398
ColePal <colepalffy1@gmail.com>
2025-08-01 19:20:46 +1200

    updated models to include correctedwords and feedback

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,12 +1,27 @@
 from django.db import models
+from datetime import date, time, datetime
+from django.utils import timezone
+
+
+#PLACEHOLDER UNTIL I WORK OUT DJANGOS SESSION AND USER SYSTEM
+class User(models.Model):
+    username = models.CharField(max_length=6, null=False,primary_key=True)
+    first_name = models.CharField(max_length=50, null=False)
+    last_name = models.CharField(max_length=50, null=False)
+    email = models.EmailField(max_length=50)
+
+class Session(models.Model):
+    id = models.CharField(max_length=20, null=False, primary_key=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
 
 # Create your models here.
 
-class SpellCorrection(models.Model):
+class CorrectionRequest(models.Model):
     # keeping track of corrections
+    session_id = models.ForeignKey(Session, on_delete=models.CASCADE)
     original_text = models.TextField()
-    corrected_text = models.TextField()
-    language = models.CharField(max_length=10, default='en')
+    received_text = models.TextField()
+    language = models.CharField(max_length=5, default='en')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -14,3 +29,22 @@ class SpellCorrection(models.Model):
         if len(self.original_text) > 50:
             preview += "..."
         return preview
+
+class CorrectedWord(models.Model):
+    id = models.AutoField(primary_key=True)
+    query_id = models.ForeignKey(CorrectionRequest, on_delete=models.CASCADE)
+    incorrect_word = models.CharField(max_length=30)
+    corrected_word = models.CharField(max_length=30)
+
+    def __str__(self):
+        preview = self.corrected_word[:20]
+        if len(self.corrected_word) > 20:
+            preview += "..."
+        return preview
+
+class WordFeedback(models.Model):
+    id = models.AutoField(primary_key=True)
+    word_id = models.ForeignKey(CorrectedWord, on_delete=models.CASCADE)
+    accepted = models.BooleanField()
+    feedback = models.CharField(max_length=200, null=False)
+
